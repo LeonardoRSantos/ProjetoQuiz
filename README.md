a151ea3fe915fd08c827c577f4a8816cec62d200
Leonardo <leonardoworkbench@gmail.com>
2021-11-30 14:26:16 -0300

    Projeto Quiz Finalizado

---
+++ README.md
@@ -0,0 +1,22 @@
+# Quiz Online ISL WYDEN
+
+### Projeto Quizz Bem Estar ISL WYDEN
+
+#### Linguagens e tecnologias usadas:
+- HTML
+- MaterializeCSS
+- JavaScript
+- PWA
+- Zeit.co (Hospedagem)
+
+#### Algumas características
+- Tema Dark/Light
+- Adicione quizzes automatizadamente
+- Salve seus resultados
+
+### Screenshots
+<p align="center">
+	<img src="images/dark_theme.png" alt="Quizzes" title="Quiz Online ISL WYDEN">
+  <img src="images/light_theme.png" alt="Quizzes" title="Quiz Online ISL WYDEN">
+</p>
+
---
+++ src/js/questions.js
@@ -0,0 +1,75 @@
+"use strict"
+
+const
+	names = [
+		'Bem Estar - ISL WYDEN'
+	
+	],
+	questions = [
+		[
+			'Você está se sentindo mal hoje?',
+			'Você dormiu mal pela noite ou dia anterior?',
+			'Você está sentindo alguma dor(cabeça, pernas,braços,juntas ou outras dores) hoje?',
+			'Você consumiu bebida alcoólica nas últimas 12 Horas?',
+			'Você está utilizando algum medicamento controlado?',
+			'Você está se sentindo estressado ou agitado nos últimos dias, incluindo hoje?',
+			'Você está se sentindo triste ou deprimido nos últimos tempos, incluindo hoje?',
+			'Você está passando por dificuldades familiares?',
+			'Você está passando por dificuldades no trabalho?',
+			'Você está passando por dificuldades financeiras?'
+		]
+		
+	],
+	
+	answers = [
+		[
+			'Sim',
+			'Sim',
+			'Sim',
+			'Sim',
+			'Sim',
+			'Sim',
+			'Sim',
+			'Sim',
+			'Sim',
+			'Sim',
+			
+			
+		],
+				
+	],
+	fakeAnswers = [
+		[
+			[
+				'Não'
+			],
+			[
+				'Não'
+			],
+			[
+				'Não'
+			],
+			[
+				'Não'
+			],
+			[
+				'Não'
+			],
+			[
+				'Não'
+			],
+			[
+				'Não'
+			],
+			[
+				'Não'
+			],
+			[
+				'Não'
+			],
+			[
+				'Não'
+			]
+		],
+		
+	]
